979e367e6bfc0f9ee2f51ba556463ca5b57fb33a
Matthieu Barbet @ Gisaia <matthieu.barbet@gisaia.com>
2017-07-05 16:57:04 +0200

    wip

--- src/contributors/HistogramContributor.ts
+++ src/contributors/HistogramContributor.ts
@@ -1,16 +1,20 @@
 
 import { Subject } from 'rxjs/Subject';
 import { CollaborativesearchService } from 'arlas-web-core';
+
 export class HistogramContributor {
     constructor(
         private valueChangedEvent: Subject<any>,
         private chartData: Subject<any>,
         private collaborativeSearcheService: CollaborativesearchService) {
         this.valueChangedEvent.subscribe(value => this.collaborativeSearcheService.setFilter(this, value))
-        this.collaborativeSearcheService.collaboraticeSubject.subscribe(value => {
-            let data = this.collaborativeSearcheService.searchButNot(value)
-            chartData.next(value)
+        this.collaborativeSearcheService.changeSubject.subscribe(value=>{
+            if(value.contributor===this){
+                return
+            }else{
+                let data = this.collaborativeSearcheService.searchButNot(value.contributor)
+                this.chartData.next(data)
+            }
         })
-
     }
 }
