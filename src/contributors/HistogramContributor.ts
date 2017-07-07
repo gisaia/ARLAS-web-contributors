bbf6284e08197261e3be1f6ec2dc30d983e922fe
Matthieu Barbet @ Gisaia <matthieu.barbet@gisaia.com>
2017-07-07 17:37:31 +0200

    connect to ARLAS

--- src/contributors/HistogramContributor.ts
+++ src/contributors/HistogramContributor.ts
@@ -1,18 +1,40 @@
 
 import { Subject } from 'rxjs/Subject';
 import { CollaborativesearchService } from 'arlas-web-core';
+import { Observable } from "rxjs/Observable";
+import { ArlasAggregation } from "api-arlas/model/arlasAggregation";
+export  interface timelineOutput{
+    endvalue:Date
+    startvalue:Date
+
+
+}
 
 export class HistogramContributor {
     constructor(
-        private valueChangedEvent: Subject<any>,
+        private valueChangedEvent: Subject<timelineOutput>,
         private chartData: Subject<any>,
         private collaborativeSearcheService: CollaborativesearchService) {
-        this.valueChangedEvent.subscribe(value => this.collaborativeSearcheService.setFilter(this, value))
-        this.collaborativeSearcheService.changeSubject.subscribe(value=>{
-            if(value.contributor===this){
+        this.valueChangedEvent.subscribe(value => {
+            let data = {contributor: this, eventType: {}, detail: value}
+            console.log(value)
+            this.collaborativeSearcheService.setFilter(this, data)
+        
+    })
+        this.collaborativeSearcheService.collaborationBus.subscribe(value => {
+            if (value.contributor === this) {
+                let data : Observable<ArlasAggregation>  = this.collaborativeSearcheService.searchButNot(value)
+                let dataTab = new Array<any>()               
+                 data.subscribe(value=>{
+                     value.elements.forEach(element=>{
+                        dataTab.push({key:element.key,value:element.elements[0].metric.value})
+                     })
+                     this.chartData.next(dataTab)
+                })
+                
                 return
-            }else{
-                let data = this.collaborativeSearcheService.searchButNot(value.contributor)
+            } else {
+                let data = this.collaborativeSearcheService.searchButNot(value)
                 this.chartData.next(data)
             }
         })
