b7adbcb47debbed692a4426e76b8c2c04e37f3a1
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2017-07-24 14:37:44 +0200

    export SearchContributor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export {TimelineContributor} from './contributors/TimelineContributor';
-export {TableContributor} from './contributors/TableContributor';
-export {MapContributor} from './contributors/MapContributor';
+export { SearchContributor } from './contributors/SearchContributor';
+export { TimelineContributor } from './contributors/TimelineContributor';
+export { TableContributor } from './contributors/TableContributor';
+export { MapContributor } from './contributors/MapContributor';
 
