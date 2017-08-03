68cd172024eb8cf2d655be96049d47d2a4f34777
Matthieu Barbet @ Gisaia <matthieu.barbet@gisaia.com>
2017-08-03 16:00:30 +0200

    externalize component fiel of timeline contributor

--- src/contributors/TimelineContributor.ts
+++ src/contributors/TimelineContributor.ts
@@ -13,42 +13,42 @@ export enum DateType {
 }
 
 export class TimelineContributor extends Contributor {
+    private valueChangedEvent: Subject<any>;
+    private chartData: Subject<any>;
+
     constructor(
         identifier: string,
         private displayName: string,
-        private valueChangedEvent: Subject<any>,
-        private chartData: Subject<any>,
         private collaborativeSearcheService: CollaborativesearchService,
-        configService: ConfigService,
-        dateType: DateType) {
+        configService: ConfigService
+    ) {
         super(identifier, configService);
-        const aggregationModel: AggregationModel = this.getConfigValue('aggregationmodel');
-        const aggregationsModels = new Array<AggregationModel>();
-        aggregationsModels.push(aggregationModel);
-        const filter: Filter = {};
-        this.plotChart(aggregationsModels);
-        this.valueChangedEvent.subscribe(
-            value => {
-                const endDate = new Date(value.endvalue);
-                const startDate = new Date(value.startvalue);
-                let multiplier = 1;
-                if (dateType === DateType.second) {
-                    multiplier = 1000;
-                }
-                const filterValue: Filter = {
-                    before: endDate.valueOf() / 1 * multiplier,
-                    after: startDate.valueOf() / 1 * multiplier
-                };
-                this.updateAndSetCollaborationEvent(this.identifier, filterValue);
-            },
-            error => { this.collaborativeSearcheService.collaborationErrorBus.next(error); });
+    }
 
-        this.collaborativeSearcheService.collaborationBus.subscribe(value => {
-            if (value.contributorId !== this.identifier) {
-                this.plotChart(aggregationsModels, this.identifier);
-            }
-        },
-            error => { this.collaborativeSearcheService.collaborationErrorBus.next(error); });
+    public getValueChangedEvent() {
+        return this.valueChangedEvent
+    }
+
+    public setValueChangedEvent(valueChangedEvent: Subject<any>, dateType) {
+        if (valueChangedEvent != undefined) {
+            this.valueChangedEvent = valueChangedEvent;
+            this.initValueChangeEvent(dateType);
+        } else {
+            this.valueChangedEvent = null;
+        }
+    }
+
+    public getCharData() {
+        return this.chartData;
+    }
+
+    public setCharData(chartData: Subject<any>) {
+        if (chartData != undefined) {
+            this.chartData = chartData;
+            this.initChartDataValue();
+        } else {
+            this.chartData = null;
+        }
     }
 
     public getFilterDisplayName(): string {
@@ -69,13 +69,8 @@ export class TimelineContributor extends Contributor {
     }
 
     private plotChart(aggregationsModels: Array<AggregationModel>, contributorId?: string) {
-        let data;
         const aggregations: Aggregations = { aggregations: aggregationsModels };
-        if (contributorId) {
-            data = this.collaborativeSearcheService.resolveButNot([eventType.aggregate, aggregations], contributorId);
-        } else {
-            data = this.collaborativeSearcheService.resolveButNot([eventType.aggregate, aggregations]);
-        }
+        const data = this.collaborativeSearcheService.resolveButNot([eventType.aggregate, aggregations], contributorId);
         const dataTab = new Array<any>();
         data.subscribe(value => {
             if (value.totalnb > 0) {
@@ -84,6 +79,52 @@ export class TimelineContributor extends Contributor {
                 });
             }
             this.chartData.next(dataTab);
+        },
+            error => { this.collaborativeSearcheService.collaborationErrorBus.next(error); });
+    }
+
+    private initValueChangeEvent(dateType) {
+        const aggregationModel: AggregationModel = this.getConfigValue('aggregationmodel');
+        const field: string = aggregationModel.field;
+        this.valueChangedEvent.subscribe(
+            value => {
+
+                let end = value.endvalue;
+                let start = value.startvalue;
+                let toto = new Date();
+                if ((typeof end.getMonth === 'function') && (typeof start.getMonth === 'function')) {
+                    const endDate = new Date(value.endvalue);
+                    const startDate = new Date(value.startvalue);
+                    let multiplier = 1;
+                    if (dateType === DateType.second) {
+                        multiplier = 1000;
+                    }
+                    end = endDate.valueOf() / 1 * multiplier,
+                        start = startDate.valueOf() / 1 * multiplier
+                }
+
+                const gt: string = field + ":gt:" + start
+                const lt: string = field + ":lt:" + end
+
+                const filterValue: Filter = {
+                    f: [gt, lt]
+
+                };
+                this.updateAndSetCollaborationEvent(this.identifier, filterValue);
+            },
+            error => { this.collaborativeSearcheService.collaborationErrorBus.next(error); });
+    }
+
+    private initChartDataValue() {
+        const aggregationModel: AggregationModel = this.getConfigValue('aggregationmodel');
+        const aggregationsModels = new Array<AggregationModel>();
+        aggregationsModels.push(aggregationModel);
+        this.plotChart(aggregationsModels, this.identifier);
+        this.collaborativeSearcheService.collaborationBus.subscribe(value => {
+            if (value.contributorId !== this.identifier) {
+                console.log
+                this.plotChart(aggregationsModels, this.identifier);
+            }
         },
             error => { this.collaborativeSearcheService.collaborationErrorBus.next(error); });
     }
