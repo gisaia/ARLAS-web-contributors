b1d3c231fc422ee6494d47b35b64d9e1dd5d337b
Matthieu Barbet @ Gisaia <matthieu.barbet@gisaia.com>
2017-10-04 13:53:48 +0200

    export models

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ export { HistogramContributor } from './contributors/HistogramContributor';
 export { ResultListContributor } from './contributors/ResultListContributor';
 export { MapContributor } from './contributors/MapContributor';
 export { ChipsSearchContributor } from './contributors/ChipsSearchContributor';
+export { Action, ProductIdentifier, triggerType } from './models/models';
+
 
 
