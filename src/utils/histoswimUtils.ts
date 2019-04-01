142f6851b28cad9c04e6cc1cb6d6f18b56c05f3c
MohamedHamou <mohamed.hamou@gisaia.com>
2019-04-01 14:48:16 +0200

    Remove unused [dataType] attribute from Histogram contributor

--- src/utils/histoswimUtils.ts
+++ src/utils/histoswimUtils.ts
@@ -17,9 +17,9 @@
  * under the License.
  */
 
-import { Collaboration, CollaborativesearchService, projType } from 'arlas-web-core';
-import { SelectedOutputValues, DataType, DateExpression } from '../models/models';
-import { Expression, Filter, Aggregation, Search, Interval } from 'arlas-api';
+import { Collaboration, CollaborativesearchService } from 'arlas-web-core';
+import { SelectedOutputValues, DateExpression } from '../models/models';
+import { Expression, Filter, Aggregation, Interval } from 'arlas-api';
 
 export function getvaluesChanged(values: SelectedOutputValues[],
     field: string,
@@ -72,8 +72,7 @@ export function getvaluesChanged(values: SelectedOutputValues[],
 }
 
 export function getSelectionToSet(data: Array<{ key: number, value: number }> | Map<string, Array<{ key: number, value: number }>>,
-    collaboration: Collaboration,
-    dataType: DataType,
+    collaboration: Collaboration
 ): any[] {
     let intervalListSelection;
     let intervalSelection;
