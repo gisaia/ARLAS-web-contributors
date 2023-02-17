dbdc40acaf487cb7b54508d7545fd461c0aff51c
Mohamed Hamou <mohamed.hamou@gisaia.com>
2023-02-17 17:53:10 +0100

    Update detailed histogram

--- src/contributors/HistogramContributor.ts
+++ src/contributors/HistogramContributor.ts
@@ -209,7 +209,7 @@ export class HistogramContributor extends Contributor {
 
     /**
      * @override Sets the data update state for the contributor itself and its corresponding detailed histogram
-     * if it exists. This garantees a coherence of the 'update state' for both histogram and its detailed histogram
+     * if it exists. This guarantees a coherence of the 'update state' for both histogram and its detailed histogram
      */
     public set updateData(value) {
         super.updateData = value;
