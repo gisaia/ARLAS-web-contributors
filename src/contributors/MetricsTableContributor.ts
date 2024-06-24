84fa30730d163170508c8770ef2eb37f2619142d
MAudelGisaia <mickael.audel@gisaia.com>
2024-06-24 22:17:54 +0200

    feat: find sort to keep the correct order.

--- src/contributors/MetricsTableContributor.ts
+++ src/contributors/MetricsTableContributor.ts
@@ -22,11 +22,16 @@ import {
     ConfigService, Contributor, OperationEnum, projType
 } from 'arlas-web-core';
 import { AggregationResponse } from 'arlas-api';
-import { MetricsVectors, MetricsTableConfig, MetricsTable, MetricsVector,
-    MetricsTableSortConfig, MetricsTableRow, MetricsTableHeader } from '../models/metrics-table.config';
-import { Observable, forkJoin, map, mergeMap, of } from 'rxjs';
+import {
+    MetricsVectors,
+    MetricsTableConfig,
+    MetricsTable,
+    MetricsTableHeader,
+    MetricsTableRow, MetricsTableData, MetricsTableSortConfig, MetricsVector
+} from '../models/metrics-table.config';
 import jsonSchema from '../jsonSchemas/metricsTableContributorConf.schema.json';
-import { aggregationResponseList } from 'models/mock-metrics';
+import { Observable, forkJoin, map, of, mergeMap } from 'rxjs';
+import { aggregationResponseList } from '../models/mock-metrics';
 
 export interface MetricsTableResponse {
     collection: string;
