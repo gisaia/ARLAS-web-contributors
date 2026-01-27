a4d89156b36fcb74bfd1b18ba5d5c88031392b77
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2026-01-27 15:37:54 +0100

    ci: release fix

--- release-actions.sh
+++ release-actions.sh
@@ -90,7 +90,7 @@ releaseProd(){
         echo "  -- tagged as ${STAGE_LOCAL}"
         npm publish --tag=${STAGE_LOCAL}
     else
-        npm publish --tag=27.0.x
+        npm publish
     fi
     cd ..
     rm -rf dist
