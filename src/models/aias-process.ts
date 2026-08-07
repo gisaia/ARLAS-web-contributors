bc15c665b1cb2a11ab141ffab4c200708552f804
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2026-08-07 16:49:22 +0200

    chore: remove unnecessary TODO

--- src/models/aias-process.ts
+++ src/models/aias-process.ts
@@ -39,9 +39,7 @@ export interface Task {
   started: number;
   finished?: number;
   updated?: number;
-  // TODO: unsure it is sent
   progress?: number;
-  // TODO: unsure it is sent
   links?: any;
   resourceID: string;
 }
